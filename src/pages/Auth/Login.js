26c36d03ed9cb8c319cbe7501390678a3af2b7c3
Popefrancis24 <francisogbonna24@gmail.com>
2021-12-21 19:30:53 +0100

    added footer to the form

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -2,11 +2,22 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import {FaArrowLeft} from "react-icons/fa";
 import images from "../../components/images";
+import {useForm} from "react-hook-form";
 
 
 const Login = () => {
     const [student, setStudent] = useState(true);
-  return(
+    const {register, handleSubmit, formState:{errors},} = useForm();
+
+    const studentLogin = (data) =>{
+      console.log(data)
+    }
+
+    const staffLogin = (data) =>{
+      console.log(data)
+    }
+ 
+    return(
     <Wrap>
       <div>
         <div className="head-tag">
@@ -15,32 +26,47 @@ const Login = () => {
           <img className="img-fluid" src={images.logo2} alt=""/>
         </div>
         {student === true? 
-            <form>
+            <form onSubmit={handleSubmit(studentLogin)}>
               <p>Are you a Staff? <span onClick={()=>setStudent(false)}>Login Here</span></p>
               <div className="form-group">
                 <label>Reg No:</label>
-                <input className="form-control" type="text" />
+                <input className="form-control" type="text" name="regNo"
+                  {...register('regNo',{required:true})}
+                />
+                {errors.regNo && <p className="text-danger">Enter Reg.</p>}
               </div>
               <div className="form-group">
                 <label>Password:</label>
-                <input className="form-control" type="password" />
+                <input className="form-control" type="password" name="password"
+                  {...register('password',{required:true})}
+                />
+                {errors.password && <p className="text-danger">Enter password.</p>}
               </div>
               <button className="btn btn-block btn-primary">Login</button>
             </form>
            :
-            <form>
+            <form onSubmit={handleSubmit(staffLogin)}>
             <p>Are you a Student? <span onClick={()=>setStudent(true)}>Login Here</span></p>
             <div className="form-group">
               <label>Username:</label>
-              <input className="form-control" type="text" />
+              <input className="form-control" type="text" name="username"
+                {...register('username',{required:true})}
+              />
+              {errors.username && <p className="text-danger">User Name is required.</p>}
             </div>
             <div className="form-group">
               <label>Password:</label>
-              <input className="form-control" type="password" />
+              <input className="form-control" type="password" name="password"
+                {...register('password',{required:true})}
+              />
+              {errors.password && <p className="text-danger">Password is required.</p>}
             </div>
             <button className="btn btn-block btn-primary">Login</button>
         </form>
         }
+        <div className="footer">
+          <p>&copy;2021 Godfrey Okoye University. All Rights Reserved</p>
+        </div>
       </div>
     </Wrap>
   )
@@ -87,6 +113,16 @@ const Wrap = styled.div `
         font-weight:bold;
         color:#0077b6;
       }
+    }
+  }
+
+  .footer{
+    background:#17418D;
+    color:#fff;
+    padding: 15px;
+    p{
+      font-size:14px;
+      text-align:center;
     }
   }
 `;
