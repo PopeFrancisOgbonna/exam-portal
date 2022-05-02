0f2551bcbcae26454377879aa9a3bd5d66ac42a8
Popefrancis24 <francisogbonna24@gmail.com>
2022-05-02 10:19:35 +0100

    added remote url

--- src/components/middleware/baseUrl.js
+++ src/components/middleware/baseUrl.js
@@ -2,6 +2,6 @@ const remote ="https://e-grade-server.herokuapp.com";
 const local = "http://localhost:3020";
 const api = {
   remoteUrl:remote,
-  localUrl:local,
+  localUrl:remote,
 }
 export default api;
